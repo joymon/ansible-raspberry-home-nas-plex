93f81c8ebb44098734807ad07b112576b2e42730
Joy George Kunjikkuru <joymon@gmail.com>
2023-12-04 11:07:18 -0500

    Added support to send mails

--- files/nas-sync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rsync /mnt/primary-exthdd/audio/ /mnt/backup-exthdd/audio/ -av --progress --delete --out-format="%t %f %''b" >> "/var/log/j3dnas/$(date +%F)/Media.log" 2>&1
-#rsync /media/primary-exthdd/media/ /mnt/backup-exthdd/media/ -av --progress --delete --out-format="%t %f %''b" >> "/var/log/j3dnas/$(date +%F)/Media.log" 2>&1
