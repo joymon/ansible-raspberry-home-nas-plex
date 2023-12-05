fd19c77879f642b989a28a0579eae3e12955c4fa
Joy George Kunjikkuru <joymon@gmail.com>
2023-12-05 17:14:46 -0500

    Fixes #4 #3

--- files/nas-rsync.sh
+++ files/nas-rsync.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir "/var/log/j3dnas/$(date +%F)" -p
 rsync /mnt/primary-exthdd/audio/ /mnt/backup-exthdd/audio/ -av --progress --delete --out-format="%t %f %''b" >> "/var/log/j3dnas/$(date +%F)/audio.log" 2>&1
-rsync /mnt/primary-exthdd/media/ /mnt/backup-exthdd/media/ -av --progress --delete --out-format="%t %f %''b" >> "/var/log/j3dnas/$(date +%F)/Media.log" 2>&1
+rsync /mnt/primary-exthdd/media/ /mnt/backup-exthdd/media/ -av --progress --delete --out-format="%t %f %''b" >> "/var/log/j3dnas/$(date +%F)/media.log" 2>&1
+
 /usr/local/bin/nas-rsync-email.sh
